bb1e731830dba13e322b9fa2ee40a7590140c681
andrews <andriy.shevtsov@masterofcode.com>
2017-12-20 10:42:52 +0200

    Implement row sorting.

--- homeworks/andrew.shevtsov_andrewsdv/homework5/homework5.go
+++ homeworks/andrew.shevtsov_andrewsdv/homework5/homework5.go
@@ -5,9 +5,23 @@ import (
 	"math/rand"
 	"github.com/MastersAcademy/go-course-2017/homeworks/andrew.shevtsov_andrewsdv/homework5/figure"
 	"time"
+	"sort"
 )
 
+type SortableRow []figure.Figure
+
+func (it SortableRow) Len() int {
+		return len(it)
+	}
+func (it SortableRow) Swap(i, j int) {
+		it[i], it[j] = it[j], it[i]
+	}
+func (it SortableRow) Less(i, j int) bool {
+		return (it[i]).Weight() > (it[j]).Weight()
+	}
+
 var boxSize int
+var shakenCorner int
 var box [][]figure.Figure
 var figures = []figure.Figure{figure.Cube{}, figure.Pyramid{}, figure.Cone{}}
 
@@ -15,6 +29,9 @@ func main() {
 	askBoxSize()
 	createBox()
 	fillBox()
+	printBox()
+	askShakenCorner()
+	sortBox(shakenCorner)
 	printBox()
 }
 
@@ -56,4 +73,40 @@ func getRandomFigure() figure.Figure {
 
 	randomIndex := randomizer.Intn(cap(figures))
 	return figures[randomIndex]
+}
+
+func askShakenCorner() {
+	fmt.Println()
+	fmt.Println("Corners map: ")
+	fmt.Println("12")
+	fmt.Println("34")
+	fmt.Println()
+
+	fmt.Print("Enter which corner to shake around: ")
+	fmt.Scanln(&shakenCorner)
+	fmt.Println()
+}
+
+func sortBox(shakenCorner int) {
+	sortableRow := make(SortableRow, boxSize)
+	for width := 0; width < boxSize; width = width + 1 {
+		for height := 0; height < boxSize; height = height + 1 {
+			sortableRow[height] = box[width][height]
+		}
+		sort.Sort(sortableRow)
+
+		if shakenCorner == 2 || shakenCorner == 4 {
+			reverseSlice(sortableRow)
+		}
+
+		for height := 0; height < boxSize; height = height + 1 {
+			box[width][height] = sortableRow[height]
+		}
+	}
+}
+func reverseSlice(sortableRow SortableRow) {
+	for i := len(sortableRow)/2 - 1; i >= 0; i-- {
+		opp := len(sortableRow) - 1 - i
+		sortableRow[i], sortableRow[opp] = sortableRow[opp], sortableRow[i]
+	}
 }
